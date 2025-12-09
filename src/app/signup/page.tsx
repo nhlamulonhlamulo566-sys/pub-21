211533c928c6ab02cd4b202aa616d9f6c93afeae
nhlamulonhlamulo566-sys <nhlamulonhlamulo566-sys@github.com>
2025-12-09 08:41:53 +0200

    Disable signup - administrator will manage user creation

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,6 @@
-import { SignupForm } from '@/components/auth/signup-form';
+import { redirect } from 'next/navigation';
 
 export default function SignupPage() {
-  return (
-    <div className="flex min-h-screen w-full items-center justify-center bg-background p-4">
-      <SignupForm />
-    </div>
-  );
+  // Signup is disabled - administrator will manage user creation
+  redirect('/login');
 }
