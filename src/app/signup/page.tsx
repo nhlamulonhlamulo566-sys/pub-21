5b52fc5d3b7f7437fddb8e22d45f1bb4d373eb78
nhlamulonhlamulo566-sys <nhlamulonhlamulo566-sys@github.com>
2025-12-10 15:30:12 +0200

    Remove signup and server-side user action files

--- src/app/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignupForm } from '@/components/auth/signup-form';
-
-export default function SignupPage() {
-  return (
-    <div className="flex min-h-screen w-full items-center justify-center bg-background p-4">
-      <SignupForm />
-    </div>
-  );
-}
